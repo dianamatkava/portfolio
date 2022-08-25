2f32e6af8ea7234d845ad3cf6a76654dce59c0d7
dianaGera <dianamatkava@gmail.com>
2022-08-25 22:13:11 +0200

    added smtp for contact form

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, render_template
+from flask_mail import Mail, Message
+import os
+from distutils.util import strtobool
+from dotenv import load_dotenv
+load_dotenv()
+
 
 app = Flask(__name__)
 
+app.config['MAIL_SERVER']= os.getenv('MAIL_SERVER')
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_USE_TLS'] = strtobool(os.getenv('MAIL_USE_TLS'))
+app.config['MAIL_USE_SSL'] = strtobool(os.getenv('MAIL_USE_SSL'))
+
+
+mail = Mail(app)
+
 @app.route('/')
 def home():
+    # print(app.config['MAIL_USE_SSL'], app.config['MAIL_USE_TLS'], 'iiiiiiiiiiiiiiiii')
+    # msg = Message("TEST subject", sender='dianamatkava@gmail.com', recipients=['dianamatkava7@gmail.com'])
+    # msg.body = 'TEST body'
+    # mail.send(msg)
     return render_template('main.html', **{'title': 'Home'})
 
 
@@ -17,3 +37,6 @@ def contact():
 @app.route('/about')
 def about():
     return render_template('about.html', **{'title': 'About'})
+
+
+
